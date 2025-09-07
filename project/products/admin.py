86a1bf58c3216c8f46fbc7357c182e2273ac57e0
rwan01 <rwanmatter1@gmail.com>
2025-09-07 21:32:32 +0300

    del

--- project/products/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin # type: ignore
-from .models import Product
-
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ("name", "price", "stock", "created_at")
-
-admin.site.register(Product)
