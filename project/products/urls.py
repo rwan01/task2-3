007ca67fecf66ad59f1908439a1764730ffbf4d0
rwan01 <rwanmatter1@gmail.com>
2025-09-09 02:20:18 +0300

    del

--- project/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path # type: ignore
-from .views import product,products
-
-urlpatterns = [
-    path('',products, name='products'),
-    path('<int:pk>/',product, name='product'),
-]
